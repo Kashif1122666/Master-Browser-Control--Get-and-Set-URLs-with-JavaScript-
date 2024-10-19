db35de459dc3e7fa79800d409786cdbd67461058
Kashif1122666 <158280129+Kashif1122666@users.noreply.github.com>
2024-10-19 13:23:23 -0700

    Add files via upload
    Master Browser Control: Get and Set URLs with JavaScript!

---
+++ script.js
@@ -0,0 +1,24 @@
+//chapter 76: Browser control - Getting and setting the url
+
+// GET full url of the current page 
+// var currentURL = window.location.href;
+// console.log(currentURL);
+
+
+// GET domain name 
+// var domain = window.location.hostname;
+// console.log(domain);
+
+
+// GET pathname 
+// patname(e.g., /index.html):
+        //    var path = window.location.pathname;
+        //    console.log(path);
+
+
+// GET anchor/hash (different sections on page, current section)        
+    // var anchor = window.location.hash;
+    // console.log({anchor});
+
+// SET new url 
+// window.location.href = 'https://www.google.com/';
